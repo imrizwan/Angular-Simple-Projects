130fb330485f2df16b2a6ab64ec2f9136b96380e
imrizwan <razzyshaikh@gmail.com>
2019-09-09 01:50:41 +0500

    first commit

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Todo } from "src/app/models/Todo"
+import { TodoService } from 'src/app/services/todo.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+  }
+
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    // toggle in ui
+    todo.completed = !todo.completed;
+    // toggle on server
+    this.todoService.toggleCompleted(todo).subscribe((todo)=> console.log(todo));
+  }
+
+  onDelete(todo) {
+    this.deleteTodo.emit(todo)
+  }
+}
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from "../../models/Todo"
+import { TodoService } from "../../services/todo.service"
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+      // this.todos = this.todoService.getTodos()
+      this.todoService.getTodos().subscribe(todos => {
+        this.todos = todos;
+      });
+      // Observable is an asynchronous data stream
+      // and we subscribe to it
+  }
+
+  deleteTodo(todo: Todo){
+
+    // Remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+
+    // Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo){
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
+  }
+
+}
